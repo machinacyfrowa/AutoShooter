0b0386a2a7b95bb9427d1fdfee5155c52aabfb1e
pzawadzki <pzawadzki@TE-GDA-D-D36G18>
2024-02-21 11:42:58 +0100

    smooth damp camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,6 +8,8 @@ public class CameraController : MonoBehaviour
     Transform player;
     //offset kamery
     Vector3 offset;
+    //prêdkoœæ kamery
+    Vector3 cameraSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,9 @@ public class CameraController : MonoBehaviour
         //policz now¹ pozycjê kamery
         Vector3 targetPosition = player.position + offset;
         //przesuñ kamerê w kierunku celu
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+        
+        //trzecia metoda - smoothdamp
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraSpeed, 0.3f);
     }
 }
