fb5541d32e7dfb2ea85e8376e706af10ba1d8b8e
pzawadzki <pzawadzki@TE-GDA-D-D36G18>
2024-02-28 10:33:56 +0100

    delay spawn to one second

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -10,23 +10,41 @@ public class LevelManager : MonoBehaviour
     //prefab przeciwnika
     public GameObject basherPrefab;
 
+    //czas od ostatniego respawnu
+    float timeSinceSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
         //zlinkuj aktualna pozycje gracza do zmiennej transform
         player = GameObject.FindWithTag("Player").transform;
+
+        //zerujemy licznik
+        timeSinceSpawn = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //wygeneruj losow¹ pozycje
-        Vector3 randomPosition = new Vector3(Random.Range(-10, 10), 0 , Random.Range(-10, 10));
+        //dodaj do czasu od ostatniego spawnu czas od ostatniej klatki (ostatni update())
+        timeSinceSpawn += Time.deltaTime;
 
-        //dodaj do niej pozycje gracza tak, aby nowe wspó³rzêdne by³y pozycj¹ wzglêdem gracza
-        randomPosition += player.position;
+        //je¿eli d³u¿ej ni¿ jedna sekunda
+        if(timeSinceSpawn > 1)
+        {
+            //wygeneruj losow¹ pozycje
+            Vector3 randomPosition = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
 
-        //stworz nowego przeciwnika z istniej¹cego prefaba, na pozycji randomPosition z rotacj¹ domyœln¹
-        Instantiate(basherPrefab, randomPosition, Quaternion.identity);
+            //dodaj do niej pozycje gracza tak, aby nowe wspó³rzêdne by³y pozycj¹ wzglêdem gracza
+            randomPosition += player.position;
+
+            //stworz nowego przeciwnika z istniej¹cego prefaba, na pozycji randomPosition z rotacj¹ domyœln¹
+            Instantiate(basherPrefab, randomPosition, Quaternion.identity);
+
+            //wyzeruj licznik
+            timeSinceSpawn = 0;
+        }
+
+        
     }
 }
