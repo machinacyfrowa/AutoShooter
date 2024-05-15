09c8c338bd5c14bebdd54ca4f9a8f1bebaf95201
pzawadzki <pzawadzki@TE-GDA-D-D36G18>
2024-05-15 12:33:00 +0200

    ignore collistions between player ammo and player body

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -72,6 +72,7 @@ public class WeaponController : MonoBehaviour
             }
          
         }
+        //zwiêksz czas od ostatniego strza³u
         timeSinceLastFire += Time.deltaTime;
 
     }
